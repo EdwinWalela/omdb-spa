208f73c7e211c6383d5a90be1a600724d5506442
edwinwalela <edwinwalela@gmail.com>
2024-01-18 11:22:52 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/Landing'
 
 function App() {
   return (
     <>
-    <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<></>} />
+      </Routes>
     </>
   )
 }

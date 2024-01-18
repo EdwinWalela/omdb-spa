0f243e4efcf212cc855a2a821995ad930b2ddbe5
edwinwalela <edwinwalela@gmail.com>
2024-01-18 12:04:48 +0300

    navbar style

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
   return (
-    <div>
-      Movix
-    </div>
-  )
+		<div className="flex shadow-md md:px-5 py-3">
+			<div>
+				<h1 className="text-2xl md:pl-0 pl-3 text-blue-500 font-medium">
+					<Link to="/">Movix</Link>
+				</h1>
+			</div>
+		</div>
+	);
 }
 
 export default NavBar;

7bf7e79695cab0c93ae52ec289ebe19dffb21f36
Edwin Walela <edwinwalela@gmail.com>
2024-01-18 11:37:30 +0300

    define project structure (#2)
    * install tailwind
    
    * add router
    
    * create not found page
    
    * align footer and nav

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div className="grow">
+      Landing Page
+    </div>
+  )
+}
+
+export default LandingPage;
---
+++ src/pages/Notfound/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const NotFoundPage = () => {
+  return (
+   <>
+    <h1>Page Not Found</h1>
+    <Link to="/">Return Home</Link>
+   </>
+  )
+}
+
+export default NotFoundPage;

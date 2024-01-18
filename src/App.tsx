724cee4ebf328c954dbd20206c795b9911b74751
edwinwalela <edwinwalela@gmail.com>
2024-01-18 11:24:57 +0300

    create not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/Landing'
+import NotFoundPage from './pages/Notfound'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<></>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   )

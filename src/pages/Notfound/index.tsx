2309e2bf3b749615f421d93eb104c47b8230971a
edwinwalela <edwinwalela@gmail.com>
2024-01-20 14:58:38 +0300

    style not found page

--- src/pages/Notfound/index.tsx
+++ src/pages/Notfound/index.tsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom"
 const NotFoundPage = () => {
   return (
    <>
-    <h1>Page Not Found</h1>
-    <Link to="/">Return Home</Link>
+    <div className="text-center flex-grow p-20">
+      <h1>Page Not Found</h1>
+      <Link 
+      to="/" 
+      className="bg-blue-500 w-1/6 lg:mx-auto text-white p-3 rounded my-10 block">
+        Return Home
+        </Link>
+    </div>
    </>
   )
 }
